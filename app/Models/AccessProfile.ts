db7ca8b257629074d42e436422697ada3dcb2a31
alanbruno1994 <76977879+alanbruno1994@users.noreply.github.com>
2021-09-15 16:29:43 -0300

    ✨ feat: created validators

--- app/Models/AccessProfile.ts
+++ app/Models/AccessProfile.ts
@@ -1,13 +1,25 @@
 import { DateTime } from 'luxon'
 import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
-
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 export default class AccessProfile extends BaseModel {
   @column({ isPrimary: true })
   public id: number
+
+  @column()
+  public level:string
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
+
+  public static getRulesValidation()
+  {
+    return schema.create({
+      level: schema.string({}, [       
+        rules.required()              
+      ])     
+    })
+  }
 }
