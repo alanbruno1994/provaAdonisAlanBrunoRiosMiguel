ddacfefa9c00f2f058e391cfff267de6a879d741
alanbruno1994 <76977879+alanbruno1994@users.noreply.github.com>
2021-09-15 15:02:06 -0300

    ✨ feat: created migrations

---
+++ database/migrations/1631728544332_bets.ts
@@ -0,0 +1,25 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Bets extends BaseSchema {
+  protected tableName = 'bets'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.integer('user_id').unsigned().references('id').inTable('users').notNullable()
+      table.integer('game_id').unsigned().references('id').inTable('games').notNullable()
+      table.string('number_choose').notNullable();
+      table.float('price_game').notNullable();
+   
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
