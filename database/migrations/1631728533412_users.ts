9177a58e4cb253028101a28a7adbb98da26f1cac
alanbruno1994 <76977879+alanbruno1994@users.noreply.github.com>
2021-09-16 11:07:57 -0300

    feat: created_recover_password

---
+++ database/migrations/1631728533412_users.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.string('email').unique().notNullable()
+      table.string('password').notNullable()
+      table.integer('access_profile_id').unsigned().references('id').inTable('access_profiles')
+      table.string('token')
+      table.timestamp('create_at_token', { useTz: true }).nullable()
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
